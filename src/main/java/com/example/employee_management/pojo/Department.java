9f9951c133120015323054a632b6194059009c3b
bakai <bakaiberdibekov@gmail.com>
2022-11-22 18:08:37 +0200

    adding entity

---
+++ src/main/java/com/example/employee_management/pojo/Department.java
@@ -0,0 +1,13 @@
+package com.example.employee_management.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Department {
+    private Integer id;
+    private String name;
+}
